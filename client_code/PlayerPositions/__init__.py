946979656ebd01e7bddb96ce7166575cfaeee1b3
jed.rutherford@btinternet.com <jed.rutherford@btinternet.com>
2023-10-15 19:16:56 +0000

    Edited PlayerPositions; Deleted PlayerPositions.RowTemplate6 and PlayerPositions.RowTemplate5

--- client_code/PlayerPositions/__init__.py
+++ client_code/PlayerPositions/__init__.py
@@ -11,121 +11,104 @@ class PlayerPositions(PlayerPositionsTemplate):
     self.form_show()
 
   def calculate_player_stats(self):
-    player_stats = []
-
-    # Retrieve all the records from the match_results table
-    records = app_tables.match_results.search()
-
-    # Count the games played and games won for each player
-    player_stats_dict = {}
-    player_winstreaks = {}  # Dictionary to track winstreaks
-    for record in records:
-        player = record['Player']
-        player_position = record['PlayerPosition']
-
-        if player not in player_stats_dict:
-            player_stats_dict[player] = {'games_played': 0, 'games_won': 0}
-            player_winstreaks[player] = {'current_winstreak': 0, 'longest_winstreak': 0}
-
-        player_stats_dict[player]['games_played'] += 1
-        if player_position == 1:
-            player_stats_dict[player]['games_won'] += 1
-            player_winstreaks[player]['current_winstreak'] += 1
-            # Check if the current winstreak exceeds the longest winstreak
-            if player_winstreaks[player]['current_winstreak'] > player_winstreaks[player]['longest_winstreak']:
-                player_winstreaks[player]['longest_winstreak'] = player_winstreaks[player]['current_winstreak']
-        else:
-            player_winstreaks[player]['current_winstreak'] = 0  # Reset winstreak on loss
-
-    # Calculate the win rate and include winstreak information for each player
-    for player, stats in player_stats_dict.items():
-        games_played = stats['games_played']
-        games_won = stats['games_won']
-        win_rate = (games_won / games_played) * 100 if games_played > 0 else 0
-        win_rate = round(win_rate, 2)
-
-        # Include winstreak information in the player statistics
-        player_stat = {
-            'Player': player,
-            'GamesPlayed': games_played,
-            'GamesWon': games_won,
-            'WinRate': win_rate,
-            'CurrentWinstreak': player_winstreaks[player]['current_winstreak'],
-            'LongestWinstreak': player_winstreaks[player]['longest_winstreak']
-        }
-        player_stats.append(player_stat)
-
-    # Sort the player_stats list by WinRate in descending order
-    player_stats.sort(key=lambda x: x['WinRate'], reverse=True)
-
-    return player_stats
-
-  def display_player_positions(self):
-    # Retrieve all the records from the match_results table
-    records = app_tables.match_results.search()
-    
-    # Create a dictionary to count player positions
-    player_positions = {}
-    
-    # Count player positions
-    for record in records:
-        player = record['Player']
-        position = record['PlayerPosition']
-        
-        if player not in player_positions:
-            player_positions[player] = {1: 0, 2: 0, 3: 0, 4: 0}  # Initialize positions with 0 counts
-        
-        player_positions[player][position] += 1
-    
-    # Sort the player_positions dictionary by the count of first places (position 1)
-    sorted_player_positions = dict(sorted(player_positions.items(), key=lambda item: item[1][1], reverse=True))
-    
-    # Create a string to display player positions
-    player_positions_text = ""
-    
-    for player, positions in sorted_player_positions.items():
-        player_positions_text += f"Player: {player}, Positions: "
-        for position in range(1, 5):  # Iterate from 1 to 4
-            count = positions.get(position, 0)  # Get the count for the position or default to 0 if not found
-            player_positions_text += f"{position}: {count}, "
-        player_positions_text = player_positions_text.rstrip(', ')  # Remove the trailing comma and space
-        player_positions_text += "\n"
-    
-    # Set the text of the label to display player positions
-    self.label_1.text = player_positions_text
-
-  def display_player_positions_data_grid(self):
+      player_stats = []
+  
       # Retrieve all the records from the match_results table
-      print("Getting the player data")
       records = app_tables.match_results.search()
+  
+      # Count the games played and games won for each player
+      player_stats_dict = {}
+      player_winstreaks = {}  # Dictionary to track winstreaks
+      for record in records:
+          player = record['Player']
+          player_position = record['PlayerPosition']
+          player_count = record['PlayerCount']  # New column for PlayerCount
+  
+          if player not in player_stats_dict:
+              player_stats_dict[player] = {'games_played': 0, 'games_won': 0, 'points': 0}  # Initialize points
+              player_winstreaks[player] = {'current_winstreak': 0, 'longest_winstreak': 0}
+  
+          player_stats_dict[player]['games_played'] += 1
+          if player_position == 1:
+              player_stats_dict[player]['games_won'] += 1
+              player_winstreaks[player]['current_winstreak'] += 1
+              # Check if the current winstreak exceeds the longest winstreak
+              if player_winstreaks[player]['current_winstreak'] > player_winstreaks[player]['longest_winstreak']:
+                  player_winstreaks[player]['longest_winstreak'] = player_winstreaks[player]['current_winstreak']
+  
+              # Calculate points based on PlayerCount
+              if player_count == 4:
+                  player_stats_dict[player]['points'] += 6
+              elif player_count == 3:
+                  player_stats_dict[player]['points'] += 5
+              elif player_count == 2:
+                  player_stats_dict[player]['points'] += 4
+              elif player_count == 1:
+                  player_stats_dict[player]['points'] += 4  # Modify this based on your rules
+  
+          else:
+              player_winstreaks[player]['current_winstreak'] = 0  # Reset winstreak on loss
+  
+      # Calculate the win rate and include winstreak information for each player
+      for player, stats in player_stats_dict.items():
+          games_played = stats['games_played']
+          games_won = stats['games_won']
+          win_rate = (games_won / games_played) * 100 if games_played > 0 else 0
+          win_rate = round(win_rate, 2)
+  
+          # Include winstreak and points information in the player statistics
+          player_stat = {
+              'Player': player,
+              'GamesPlayed': games_played,
+              'GamesWon': games_won,
+              'WinRate': win_rate,
+              'CurrentWinstreak': player_winstreaks[player]['current_winstreak'],
+              'LongestWinstreak': player_winstreaks[player]['longest_winstreak'],
+              'Points': stats['points']  # Add points to player statistics
+          }
+          player_stats.append(player_stat)
+  
+      # Sort the player_stats list by Points in descending order
+      player_stats.sort(key=lambda x: x['Points'], reverse=True)
+  
+      return player_stats
 
+  def populate_player_positions_repeating_panel(self):
+      # Retrieve all the records from the match_results table
+      records = app_tables.match_results.search()
+  
       # Create a dictionary to count player positions
       player_positions = {}
       for record in records:
           player = record['Player']
           position = record['PlayerPosition']
-
+          
           if player not in player_positions:
               player_positions[player] = {1: 0, 2: 0, 3: 0, 4: 0}
-
+          
           player_positions[player][position] += 1
-
+      
       # Sort the player_positions dictionary by the count of first places (position 1)
       sorted_player_positions = dict(sorted(player_positions.items(), key=lambda item: item[1][1], reverse=True))
-
-      # Clear the RepeatingPanel
-      self.repeating_panel_1.items = []
-      print(sorted_player_positions)
-
-      # Populate the RepeatingPanel
+      
+      # Create a list of dictionaries for the RepeatingPanel
+      data = []
       for player, positions in sorted_player_positions.items():
-          row = {'column_1': player, 'column_2': positions[1], 'column_3': positions[2], 'column_4': positions[3], 'column_5': positions[4]}
-          self.repeating_panel_1.items.append(row)
+          data.append({
+              'Player': player,
+              '1st Place': positions[1],
+              '2nd Place': positions[2],
+              '3rd Place': positions[3],
+              '4th Place': positions[4]
+          })
+      print(data)
+      # Set the RepeatingPanel items to the list of dictionaries
+      data = [{'column_1': player, 'column_2': positions[1], 'column_3': positions[2], 'column_4': positions[3], 'column_5': positions[4]} for player, positions in sorted_player_positions.items()]
+      self.repeating_panel_1.items = data
 
   
   def form_show(self, **event_args):
-      self.display_player_positions()
-      self.display_player_positions_data_grid()
+      self.populate_player_positions_repeating_panel()
 
   def button_1_click(self, **event_args):
       open_form('Home')
