6f5ea32d14eda4f06755f66db3cb70b885f74b4b
jed.rutherford@btinternet.com <jed.rutherford@btinternet.com>
2023-10-21 23:17:54 +0000

    Edited AddMatch and CommanderStats

--- client_code/AddMatch/__init__.py
+++ client_code/AddMatch/__init__.py
@@ -58,13 +58,12 @@ class AddMatch(AddMatchTemplate):
     def button_3_click(self, **event_args):
         open_form('AddCommander')
       
-    def button_4_click(self, **event_args):
+    def button_4_copy_click(self, **event_args):
         open_form('CommanderStats')
-
+ 
     def button_5_click(self, **event_args):
         open_form('PlayerPositions')
 
-  
     def get_commander_names_from_table(self):
         commander_names = []
         
@@ -100,3 +99,5 @@ class AddMatch(AddMatchTemplate):
             row_panel.get_components()[2].selected_value = None
         # Show a success message as a popup
         PlayerPositionsanvil.server.alert("Commander added successfully!", title="Success")
+
+
--- client_code/CommanderStats/__init__.py
+++ client_code/CommanderStats/__init__.py
@@ -83,17 +83,6 @@ class CommanderStats(CommanderStatsTemplate):
     
         return commander_stats
 
-   
-    def populate_data_grid(self):
-        try:
-            commander_stats = self.get_commander_stats()
-            stats_text = ""
-            for stat in commander_stats:
-                stats_text += f"{stat['Commander']}, Played: {stat['GamesPlayed']}, Won: {stat['GamesWon']}, Win Rate: {stat['WinRate']}%\n"
-    
-            self.text_box_1.text = stats_text
-        except Exception as e:
-            print(f"Error occurred while populating data grid: {str(e)}")
 
 
     def calculate_player_stats(self):
@@ -146,68 +135,6 @@ class CommanderStats(CommanderStatsTemplate):
   
       return player_stats
 
-
-    def populate_player_data_grid(self):
-        try:
-            commander_stats = self.get_commander_stats()
-            player_stats = self.calculate_player_stats()
-            stats_text = ""
-            for stat in player_stats:
-                stats_text += f"{stat['Player']}, Played: {stat['GamesPlayed']}, Won: {stat['GamesWon']}, Win Rate: {stat['WinRate']:.2f}%\n"
-    # Sort the commander_stats list alphabetically by Commander
-            commander_stats.sort(key=lambda x: x['Commander'])
-    
-            stats_text_alphabetical = ""
-            for stat in commander_stats:
-                stats_text_alphabetical += f"{stat['Commander']}, Played: {stat['GamesPlayed']}, Won: {stat['GamesWon']}, Win Rate: {stat['WinRate']}%\n"
-    
-            self.player_textbox.text = stats_text
-            self.text_box_2.text = stats_text_alphabetical
-        except Exception as e:
-            print(f"Error occurred while populating player data grid: {str(e)}")
-
-      
-    def populate_commander_played_data(self):
-        try:
-            commander_stats = self.get_commander_stats()
-    
-            # Sort the commander_stats list by the number of times a deck has been played in descending order
-            commander_stats.sort(key=lambda x: x['GamesPlayed'], reverse=True)
-    
-            stats_text = ""
-            for stat in commander_stats:
-                commander = stat['Commander']
-                games_played = stat['GamesPlayed']
-                games_won = stat['GamesWon']
-                win_rate = stat['WinRate']
-    
-                stats_text += f"{commander}, Played: {games_played}, Won: {games_won}, Win Rate: {win_rate}%\n"
-    
-            self.commander_textbox.text = stats_text
-        except Exception as e:
-            print(f"Error occurred while populating commander played data: {str(e)}")
-
-    def populate_player_data_grid(self):
-      try:
-          commander_stats = self.get_commander_stats()
-          player_stats = self.calculate_player_stats()
-          stats_text = ""
-          for stat in player_stats:
-              stats_text += f"{stat['Player']}, Played: {stat['GamesPlayed']}, Won: {stat['GamesWon']}, Win Rate: {stat['WinRate']:.2f}%\n"
-              stats_text += f"Current Winstreak: {stat['CurrentWinstreak']}, Longest Winstreak: {stat['LongestWinstreak']}\n\n"
-  
-          # Sort the commander_stats list alphabetically by Commander
-          commander_stats.sort(key=lambda x: x['Commander'])
-  
-          stats_text_alphabetical = ""
-          for stat in commander_stats:
-              stats_text_alphabetical += f"{stat['Commander']}, Played: {stat['GamesPlayed']}, Won: {stat['GamesWon']}, Win Rate: {stat['WinRate']}%\n"
-  
-          self.player_textbox.text = stats_text
-          self.text_box_2.text = stats_text_alphabetical
-      except Exception as e:
-          print(f"Error occurred while populating player data grid: {str(e)}")
-
     
     def get_colour_stats(self):
         colour_stats = []
@@ -258,149 +185,11 @@ class CommanderStats(CommanderStatsTemplate):
       except Exception as e:
             print(f"Error occurred while populating commander data grid: {str(e)}")
 
-    def populate_player_grid(self):
-      try:
-            player_stats = self.calculate_player_stats()
-            data = [{'player_name': stat['Player'], 'player_played': stat['GamesPlayed'], 'player_won': stat['GamesWon'], 'player_rate': f"{stat['WinRate']:.2f}%"} for stat in player_stats]
-            self.repeating_panel_2.items = data
-      except Exception as e:
-            print(f"Error occurred while populating player data grid: {str(e)}")
-
-    def display_winstreak(self):
-        try:
-            player_stats = self.calculate_player_stats()
-            winstreak_text = ""
-            for stat in player_stats:
-                winstreak_text += f"{stat['Player']}'s Current Winstreak: {stat['CurrentWinstreak']}, Longest Winstreak: {stat['LongestWinstreak']}\n"
-            
-            # Set the content of the win_streak rich text box
-            self.win_streak.content = winstreak_text
-            print(winstreak_text)
-        except Exception as e:
-            print(f"Error occurred while displaying winstreak: {str(e)}")
-
-
-
-          
-    def get_colour_counts(self):
-        color_counts = {'White': 0, 'Blue': 0, 'Black': 0, 'Red': 0, 'Green': 0, 'Colourless': 0}
-    
-        # Retrieve all the records from the commanders table
-        records = app_tables.commanders.search()
-    
-        # Count the colors for each commander
-        for record in records:
-            white = record['White']
-            blue = record['Blue']
-            black = record['Black']
-            red = record['Red']
-            green = record['Green']
-            colourless = record['Colourless']
-            # Update the total counts
-            color_counts['White'] += white
-            color_counts['Blue'] += blue
-            color_counts['Black'] += black
-            color_counts['Red'] += red
-            color_counts['Green'] += green
-            color_counts['Colourless'] += colourless
-    
-        return color_counts
-
-    def display_colour_count(self):
-        try:
-            color_counts = self.get_colour_counts()
-            stats_text = ""
-            for color, count in color_counts.items():
-                stats_text += f"{color}: {count}\n"
-    
-            self.colour_counts.text = stats_text
-        except Exception as e:
-            print(f"Error occurred while populating colour data: {str(e)}")
-
-
-
-  
-    def display_most_played_and_best_deck(self):
-        try:
-            records = app_tables.match_results.search()
-    
-            player_stats = {}
-            deck_play_counts = {}
-            deck_win_counts = {}
-    
-            for record in records:
-                player = record['Player']
-                deck = record['Commander']
-                player_position = record['PlayerPosition']
-    
-                if player not in player_stats:
-                    player_stats[player] = {
-                        'total_played': 0,
-                        'position_1_count': 0,
-                        'win_rate': 0,
-                        'decks': {}
-                    }
-    
-                if deck not in player_stats[player]['decks']:
-                    player_stats[player]['decks'][deck] = {
-                        'played_count': 0,
-                        'win_count': 0,
-                        'win_rate': 0
-                    }
-    
-                player_stats[player]['decks'][deck]['played_count'] += 1
-                player_stats[player]['total_played'] += 1
-    
-                if player_position == 1:
-                    player_stats[player]['decks'][deck]['win_count'] += 1
-                    player_stats[player]['position_1_count'] += 1
-    
-            for player, stats in player_stats.items():
-                games_played = stats['total_played']
-                games_won = stats['position_1_count']
-                win_rate = (games_won / games_played) * 100 if games_played > 0 else 0
-                stats['win_rate'] = win_rate
-    
-            most_played_decks = {}
-            best_performing_decks = {}
-    
-            for player, stats in player_stats.items():
-                most_played_deck = max(stats['decks'], key=lambda x: stats['decks'][x]['played_count'])
-                win_rate_most_played = (stats['decks'][most_played_deck]['win_count'] / stats['decks'][most_played_deck]['played_count']) * 100
-                played_count_most_played = stats['decks'][most_played_deck]['played_count']
-                most_played_decks[player] = (most_played_deck, played_count_most_played, win_rate_most_played)
-    
-                best_performing_deck = max(stats['decks'], key=lambda x: (stats['decks'][x]['win_count'] / stats['decks'][x]['played_count']) * 100)
-                win_rate_best_performing = (stats['decks'][best_performing_deck]['win_count'] / stats['decks'][best_performing_deck]['played_count']) * 100
-                played_count_best_performing = stats['decks'][best_performing_deck]['played_count']
-                best_performing_decks[player] = (best_performing_deck, played_count_best_performing, win_rate_best_performing)
-    
-            # Display the player's most played deck and its win rate
-            stats_text = ""
-            for player, (most_played_deck, played_count_most_played, win_rate_most_played) in most_played_decks.items():
-                best_performing_deck, played_count_best_performing, win_rate_best_performing = best_performing_decks[player]
-                stats_text += f"Player: {player}\n"
-                stats_text += f"Most Played Deck: {most_played_deck}, Played: {played_count_most_played}, Win Rate: {win_rate_most_played:.2f}%\n"
-                stats_text += f"Best Performing Deck: {best_performing_deck}, Played: {played_count_best_performing}, Win Rate: {win_rate_best_performing:.2f}%\n"
-                stats_text += "\n"
-    
-            self.player_deck.text = stats_text
-    
-        except Exception as e:
-            print(f"Error occurred while displaying player deck stats: {str(e)}")
-
 
 
     def form_show(self, **event_args):
-        self.populate_data_grid()
-        self.populate_player_data_grid()
-        self.populate_commander_played_data()
-        self.get_colour_stats()
-        self.display_most_played_and_best_deck()
-        self.display_colour_count()
         self.populate_commander_grid()
-        self.populate_player_grid()
-        self.display_winstreak()
+
 
 
     def button_1_click(self, **event_args):
